3772f282de5571a4e7a6728722d8b083254b90b3
starsshinee <lintangcahyaniputri131@gmail.com>
2026-05-16 23:01:30 +0800

    tambah nmor telpon

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('nomor_telepon')->nullable();
             $table->string('password');
             $table->enum('role', [
                 'superadmin',
